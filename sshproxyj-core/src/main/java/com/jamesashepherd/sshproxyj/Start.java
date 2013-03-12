62a15f51810d1f2aed31377ea401b21473983452
James Shepherd <spam123@JamesAshepherd.com>
2013-03-12 19:16:29 +0000

    basic password auth works

--- sshproxyj-core/src/main/java/com/jamesashepherd/sshproxyj/Start.java
+++ sshproxyj-core/src/main/java/com/jamesashepherd/sshproxyj/Start.java
@@ -2,9 +2,17 @@
 package com.jamesashepherd.sshproxyj;
 
 import java.io.IOException;
+import java.security.PublicKey;
+import java.util.Arrays;
 
 import org.apache.sshd.SshServer;
+import org.apache.sshd.client.auth.UserAuthPublicKey;
+import org.apache.sshd.common.NamedFactory;
+import org.apache.sshd.server.PasswordAuthenticator;
+import org.apache.sshd.server.PublickeyAuthenticator;
+import org.apache.sshd.server.UserAuth;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.session.ServerSession;
 import org.apache.sshd.server.shell.ProcessShellFactory;
 
 import com.jamesashepherd.start.StartException;
@@ -25,12 +33,17 @@ public class Start implements Startable {
 	/* (non-Javadoc)
 	 * @see com.jamesashepherd.start.Startable#startup()
 	 */
-	@Override
 	public void startup() throws StartException {
 		sshd = SshServer.setUpDefaultServer();
 		sshd.setPort(6667);
 		sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("/tmp/host.key"));
 		sshd.setShellFactory(new ProcessShellFactory(new String[] { "/bin/bash", "-i", "-l" }));
+		sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+			
+			public boolean authenticate(String username, String password, ServerSession session) {
+				return username.equals("jas") && password.equals("mega");
+			}
+		});
 		try {
 			sshd.start();
 		} catch (IOException e) {
@@ -41,7 +54,6 @@ public class Start implements Startable {
 	/* (non-Javadoc)
 	 * @see com.jamesashepherd.start.Startable#shutdown()
 	 */
-	@Override
 	public void shutdown() throws StartException {
 		try {
 			sshd.stop();
